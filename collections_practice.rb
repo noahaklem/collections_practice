913b7b5b41846a2a857c909449db3666de0f89fd
Noah A Klem <noahaklem@gmail.com>
2021-08-30 13:32:57 -0600

    commit tree e68c8f94a12ac7bd8bcb42f585871a330c0536a3

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(integers)
+    integers.sort
+end
 
+def sort_array_desc(integers)
+    integers.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |name|
+        name[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |word|
+        word[0] == "a"
+    end
+end
+
+def sum_array(array)
+    sum = 0
+    array.each do |num|
+        sum += num
+    end
+    sum
+end
+
+def add_s(array)
+    array.collect do |word|
+        if array[1] == word
+            word
+        else
+            word + "s"
+        end
+    end
+end
